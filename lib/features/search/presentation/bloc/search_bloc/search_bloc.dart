189ac76f457fbc847b382e04fbc3f821b964d21a
Lagil Taufik <lagiltaufik@MacBook-Air-Lagil.local>
2026-05-03 20:05:47 +0500

    feat: implemet search Bloc

--- lib/features/search/presentation/bloc/search_bloc/search_bloc.dart
+++ lib/features/search/presentation/bloc/search_bloc/search_bloc.dart
@@ -1,12 +1,133 @@
 import 'package:bloc/bloc.dart';
+import 'package:flavory/core/utils/failure/failure.dart';
+import 'package:flavory/core/utils/statuses/statuses.dart';
+import 'package:flavory/features/search/domain/entity/search_recipe_entity.dart';
+import 'package:flavory/features/search/domain/search_filter.dart';
+import 'package:flavory/features/search/domain/usecase/search_recipes_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  SearchBloc() : super(SearchInitial()) {
-    on<SearchEvent>((event, emit) {
-    });
+  final SearchRecipesUsecase _usecase;
+  SearchBloc({required SearchRecipesUsecase usecase})
+    : _usecase = usecase,
+      super(SearchState()) {
+    on<SearchRecipesEvent>(_onSearch);
+    on<LoadMoreRecipesEvent>(_onLoadMore);
+    on<UpdateFilterEvent>(_onUpdateFilter);
+  }
+
+  Future<void> _onSearch(
+    SearchRecipesEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    emit(
+      state.copyWith(
+        status: .loading,
+        query: state.query,
+        error: null,
+        hasReachedMax: false,
+        recipes: [],
+      ),
+    );
+
+    try {
+      final result = await _usecase.call(
+        query: event.query,
+        filter: state.filter,
+        offset: 0,
+      );
+      emit(
+        state.copyWith(
+          status: .success,
+          recipes: result,
+          hasReachedMax: result.length < 20,
+        ),
+      );
+    } catch (e) {
+      final message;
+      e is Failure ? message = e.message : message = e.toString();
+
+      emit(state.copyWith(status: .error, error: message));
+    }
+  }
+
+  Future<void> _onLoadMore(
+    LoadMoreRecipesEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    if (state.isLoadingMore || state.hasReachedMax) return;
+
+    emit(state.copyWith(isLoadingMore: true));
+    try {
+      final result = await _usecase.call(
+        query: state.query,
+        filter: state.filter,
+        offset: state.recipes.length,
+      );
+      emit(
+        state.copyWith(
+          recipes: [...state.recipes, ...result],
+          isLoadingMore: false,
+          hasReachedMax: result.length < 20,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: .error,
+          isLoadingMore: false,
+          error: _mapError(e),
+        ),
+      );
+    }
+  }
+
+  Future<void> _onUpdateFilter(
+    UpdateFilterEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    emit(state.copyWith(filter: event.filter));
+
+    add(SearchRecipesEvent(state.query));
+  }
+
+  Future<void> _onLoadInitialEvent(
+    LoadInitialRecipesEvent event,
+    Emitter<SearchState> emit,
+  ) async {
+    emit(
+      state.copyWith(
+        status: .loading,
+        query: "",
+        error: null,
+        recipes: [],
+        hasReachedMax: false,
+      ),
+    );
+
+    try {
+      final result = await _usecase.call(
+        query: state.query,
+        filter: state.filter,
+        offset: 0,
+      );
+      emit(
+        state.copyWith(
+          status: .success,
+          recipes: result,
+          hasReachedMax: result.length < 20,
+        ),
+      );
+    } catch (e) {
+      emit(state.copyWith(status: .error, error: _mapError(e)));
+    }
+  }
+
+  String _mapError(dynamic e) {
+    if (e is Failure) return e.message;
+    return e.toString();
   }
 }
--- lib/features/search/presentation/bloc/search_bloc/search_state.dart
+++ lib/features/search/presentation/bloc/search_bloc/search_state.dart
@@ -1,6 +1,41 @@
 part of 'search_bloc.dart';
 
 @immutable
-sealed class SearchState {}
+final class SearchState {
+  SearchState({
+    this.status = .initial,
+    this.filter = const SearchFilter(),
+    this.recipes = const [],
+    this.query = "",
+    this.error,
+    this.isLoadingMore = false,
+    this.hasReachedMax = false,
+  });
+  final Statuses status;
+  final List<SearchRecipeEntity> recipes;
+  final SearchFilter filter;
+  final String query;
+  final bool isLoadingMore;
+  final bool hasReachedMax;
+  final String? error;
 
-final class SearchInitial extends SearchState {}
+  SearchState copyWith({
+    Statuses? status,
+    List<SearchRecipeEntity>? recipes,
+    SearchFilter? filter,
+    String? query,
+    bool? isLoadingMore,
+    bool? hasReachedMax,
+    String? error,
+  }) {
+    return SearchState(
+      status: status ?? this.status,
+      recipes: recipes ?? this.recipes,
+      filter: filter ?? this.filter,
+      query: query ?? this.query,
+      isLoadingMore: isLoadingMore ?? this.isLoadingMore,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      error: error,
+    );
+  }
+}
